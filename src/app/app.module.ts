63188d76d8d66566b45e429f87934996470b6e33
licol <licol@MAURO_LUIZ>
2022-09-28 16:58:16 -0300

    Adicionar arquivos de projeto.

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp({ 
+      apiKey: "AIzaSyDMJ-_eNZh2QgSaWAmEtsn-Xt4NlJJAEMA",
+      authDomain: "fapan-app-entregas.firebaseapp.com",
+      projectId: "fapan-app-entregas",
+      storageBucket: "fapan-app-entregas.appspot.com",
+      messagingSenderId: "14291716915",
+      appId: "1:14291716915:web:dc1f58423cf0fb4f8b8c46"
+     }),
+     AngularFirestoreModule
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
